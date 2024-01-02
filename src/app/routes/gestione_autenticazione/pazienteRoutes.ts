7de2c627bc12914a8bf780d337bce16312499489
oghamSA <o.dellerba@studenti.unisa.it>
2024-01-02 19:08:29 +0100

    add post method "visualizza_caregiver"

--- src/app/routes/gestione_autenticazione/pazienteRoutes.ts
+++ src/app/routes/gestione_autenticazione/pazienteRoutes.ts
@@ -1,10 +1,14 @@
 import { Paziente } from 'app/entity/gestione_autenticazione/Paziente';
 import { PazienteService } from 'app/services/gestione_autenticazione/paziente/PazienteService';
 import { PazienteServiceInterface } from 'app/services/gestione_autenticazione/paziente/PazienteServiceInterface';
+import { CaregiverFamiliareService } from 'app/services/gestione_autenticazione/caregiver_familiare/CaregiverFamiliareService';
+import { CaregiverFamiliareServiceInterface } from 'app/services/gestione_autenticazione/caregiver_familiare/CaregiverFamiliareServiceInterface';
 import express, { Request, Response } from 'express';
 
 const router = express.Router();
 const pazienteService: PazienteServiceInterface = new PazienteService();
+const caregiverFamiliareService: CaregiverFamiliareServiceInterface =
+  new CaregiverFamiliareService();
 
 router.get('/visualizza_pazienti', async (req: Request, res: Response) => {
   try {
@@ -35,6 +39,16 @@ router.post('/dati_paziente', async (req: Request, res: Response) => {
     const paziente = await pazienteService.get(codice_fiscale);
 
     res.json(paziente);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.post('/visualizza_caregiver', async (req: Request, res: Response) => {
+  try {
+    const id: number = req.body.codice_identificativo;
+    const caregiverFamiliare = await caregiverFamiliareService.get(id);
+    res.json(caregiverFamiliare);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
