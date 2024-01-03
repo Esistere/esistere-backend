a166f28d4121217974ef55e7a4897fc501c30377
oghamSA <o.dellerba@studenti.unisa.it>
2024-01-03 17:27:34 +0100

    add classes "QuizPreliminareServiceInterface" and "QuizPreliminareService"

---
+++ src/app/services/gestione_quiz_preliminare/QuizPreliminareService.ts
@@ -0,0 +1,73 @@
+import { QuizPreliminare } from 'app/entity/gestione_quiz_preliminare/QuizPreliminare';
+import { DomandaQuizPreliminare } from 'app/entity/gestione_quiz_preliminare/DomandaQuizPreliminare';
+import { RispostaQuizPreliminare } from 'app/entity/gestione_quiz_preliminare/RispostaQuizPreliminare';
+import { QuizPreliminareServiceInterface } from './QuizPreliminareServiceInterface';
+import { QuizPreliminareDAOInterface } from 'app/dao/gestione_quiz_preliminare/QuizPreliminareDAOInterface';
+import { QuizPreliminareDAO } from 'app/dao/gestione_quiz_preliminare/QuizPreliminareDAO';
+
+export class QuizPreliminareService implements QuizPreliminareServiceInterface {
+  private quizPreliminareDAO: QuizPreliminareDAOInterface;
+
+  constructor() {
+    this.quizPreliminareDAO = new QuizPreliminareDAO();
+  }
+
+  getAll(): Promise<QuizPreliminare[]> {
+    return this.quizPreliminareDAO.getAll();
+  }
+
+  getByMed(medico: number): Promise<QuizPreliminare[]> {
+    return this.quizPreliminareDAO.getByMed(medico);
+  }
+
+  getAllDomande(): Promise<DomandaQuizPreliminare[]> {
+    return this.quizPreliminareDAO.getAllDomande();
+  }
+
+  getDomanda(id: number): Promise<DomandaQuizPreliminare> {
+    return this.quizPreliminareDAO.getDomanda(id);
+  }
+
+  get(id: number): Promise<QuizPreliminare> {
+    return this.quizPreliminareDAO.get(id);
+  }
+
+  save(quizPreliminare: QuizPreliminare): Promise<void> {
+    return this.save(quizPreliminare);
+  }
+
+  saveDomanda(domanda: DomandaQuizPreliminare): Promise<void> {
+    return this.quizPreliminareDAO.saveDomanda(domanda);
+  }
+  update(quizPreliminare: QuizPreliminare): Promise<void> {
+    return this.quizPreliminareDAO.update(quizPreliminare);
+  }
+
+  updateDomanda(domanda: DomandaQuizPreliminare): Promise<void> {
+    return this.quizPreliminareDAO.updateDomanda(domanda);
+  }
+
+  getByQuizPreliminare(
+    quizPreliminare: number
+  ): Promise<DomandaQuizPreliminare[]> {
+    return this.quizPreliminareDAO.getByQuizPreliminare(quizPreliminare);
+  }
+
+  getRispostaByPaziente(
+    paziente: string,
+    id: number
+  ): Promise<RispostaQuizPreliminare> {
+    return this.quizPreliminareDAO.getRispostaByPaziente(paziente, id);
+  }
+
+  getRisposta(id: number): Promise<RispostaQuizPreliminare> {
+    return this.quizPreliminareDAO.getRisposta(id);
+  }
+
+  saveRisposta(risposta: RispostaQuizPreliminare): Promise<void> {
+    return this.quizPreliminareDAO.saveRisposta(risposta);
+  }
+  updateRisposta(risposta: RispostaQuizPreliminare): Promise<void> {
+    return this.quizPreliminareDAO.updateRisposta(risposta);
+  }
+}
---
+++ src/app/services/gestione_quiz_preliminare/QuizPreliminareServiceInterface.ts
@@ -0,0 +1,25 @@
+import { QuizPreliminare } from 'app/entity/gestione_quiz_preliminare/QuizPreliminare';
+import { DomandaQuizPreliminare } from 'app/entity/gestione_quiz_preliminare/DomandaQuizPreliminare';
+import { RispostaQuizPreliminare } from 'app/entity/gestione_quiz_preliminare/RispostaQuizPreliminare';
+
+export interface QuizPreliminareServiceInterface {
+  getAll(): Promise<QuizPreliminare[]>;
+  getByMed(medico: number): Promise<QuizPreliminare[]>;
+  getAllDomande(): Promise<DomandaQuizPreliminare[]>;
+  getDomanda(id: number): Promise<DomandaQuizPreliminare>;
+  get(id: number): Promise<QuizPreliminare>;
+  save(quizPreliminare: QuizPreliminare): Promise<void>;
+  saveDomanda(domanda: DomandaQuizPreliminare): Promise<void>;
+  update(quizPreliminare: QuizPreliminare): Promise<void>;
+  updateDomanda(domanda: DomandaQuizPreliminare): Promise<void>;
+  getByQuizPreliminare(
+    quizPreliminare: number
+  ): Promise<DomandaQuizPreliminare[]>;
+  getRispostaByPaziente(
+    paziente: string,
+    id: number
+  ): Promise<RispostaQuizPreliminare>;
+  getRisposta(id: number): Promise<RispostaQuizPreliminare>;
+  saveRisposta(risposta: RispostaQuizPreliminare): Promise<void>;
+  updateRisposta(risposta: RispostaQuizPreliminare): Promise<void>;
+}
