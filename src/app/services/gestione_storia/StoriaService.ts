fd64ebc28761e3ca72fa35357f8b4cba8e4db8c6
oghamSA <o.dellerba@studenti.unisa.it>
2024-01-08 20:54:50 +0100

    add files StoriaService and StoriaServiceInterface

---
+++ src/app/services/gestione_storia/StoriaService.ts
@@ -0,0 +1,55 @@
+import { Storia } from 'app/entity/gestione_storia/Storia';
+import { Media } from 'app/entity/gestione_storia/Media';
+import { StoriaDAO } from 'app/dao/gestione_storia/StoriaDAO';
+import { StoriaDAOInterface } from 'app/dao/gestione_storia/StoriaDAOInterface';
+import { StoriaServiceInterface } from './StoriaServiceInterface';
+
+export class StoriaService implements StoriaServiceInterface {
+  private storiaDAO: StoriaDAOInterface;
+
+  constructor() {
+    this.storiaDAO = new StoriaDAO();
+  }
+
+  public getAll(): Promise<Storia[]> {
+    return this.storiaDAO.getAll();
+  }
+
+  public get(id: number): Promise<Storia> {
+    return this.storiaDAO.get(id);
+  }
+
+  public save(storia: Storia): Promise<void> {
+    return this.storiaDAO.save(storia);
+  }
+
+  public update(storia: Storia): Promise<void> {
+    return this.storiaDAO.update(storia);
+  }
+
+  public getByCaregiverFamiliare(
+    caregiverFamiliare: number
+  ): Promise<Storia[]> {
+    return this.storiaDAO.getByCaregiverFamiliare(caregiverFamiliare);
+  }
+
+  public getAllMedia(): Promise<Media[]> {
+    return this.storiaDAO.getAllMedia();
+  }
+
+  public getMedia(id: number): Promise<Media> {
+    return this.storiaDAO.getMedia(id);
+  }
+
+  public saveMedia(media: Media): Promise<void> {
+    return this.storiaDAO.saveMedia(media);
+  }
+
+  public updateMedia(media: Media): Promise<void> {
+    return this.storiaDAO.updateMedia(media);
+  }
+
+  public getByStoria(storia: number): Promise<Media[]> {
+    return this.storiaDAO.getByStoria(storia);
+  }
+}
